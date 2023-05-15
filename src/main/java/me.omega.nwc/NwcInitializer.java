7355dbd0c67dff302842c7a0ff45b35db4c15adf
omegaladon <businessomga@gmail.com>
2023-05-15 12:09:04 -0700

    update resources

--- src/main/java/me.omega.nwc/NwcInitializer.java
+++ src/main/java/me.omega.nwc/NwcInitializer.java
@@ -14,7 +14,7 @@ public class NwcInitializer implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Logger LOGGER = LoggerFactory.getLogger("nwc");
 
 	@Override
 	public void onInitialize() {
